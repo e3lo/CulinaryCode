1eb6f8c132922650386ae7dca7c1036ba510fb1c
Leo Heng <leoheng905@gmail.com>
2023-08-19 16:08:00 +1000

    styled navigation

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
       <nav class="navigation">
         <div class="navigation__logo">
-          <span>Culinary</span><span>Code</span>
+          <RouterLink to="/"><span class="playfair logo">Culinary</span><span class="logo">Code</span></RouterLink>
         </div>
         <div class="navigation__buttons">
           <RouterLink to="/">Home</RouterLink>
@@ -23,6 +23,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <style scoped>
   .navigation {
     display: flex;
+    align-items: center;
   }
 
   .navigation__logo {
@@ -32,6 +33,12 @@ import { RouterLink, RouterView } from 'vue-router'
   .navigation__buttons {
     display: flex;
     gap: 2em;
+  }
+
+  .logo {
+    font-weight: 700;
+    font-size: 32px;
+    color: #693232;
   }
 
 </style>
