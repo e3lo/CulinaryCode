47595300feea9c416069bdff0af5fb5983e5f1df
Leo Heng <leoheng905@gmail.com>
2023-08-19 15:48:01 +1000

    removed helloworld added links

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,20 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/trending',
+      name: 'trending',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/TrendingView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
     }
   ]
 })
